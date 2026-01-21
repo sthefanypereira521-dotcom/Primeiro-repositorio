8afa330577e52f6dff3813190e91f926d041f2b4
sthefanypereira521-dotcom <sthefanypereira521@gmail.com>
2026-01-20 21:07:09 -0300

    Calcula aumento de 15% no salário

---
+++ Python Exercícios_básicos /Exercícios 02.py
@@ -0,0 +1,5 @@
+salario = float (input (' qual e o salario do funcionario? R$ '))
+novo = salario + ( salario * 15 / 100)
+print ( ' um funcionario que ganhava R${} , com 15% de aumento, passou a receber R${}. '.format(salario, novo))
+
+
--- Python Exercícios_básicos /dobro e triplo.py
+++
@@ -1,6 +0,0 @@
-n = int(input( 'digite um numero: '))
-d = n*2
-t = n*3
-r = n **(1/2)
-print('o dobro de {} vale {}'.format(n,d))
-print('o triplo de {} vale {}.\n a raiz quadrada de {} e igual a {:.2f}'.format(n,t,n,r))
