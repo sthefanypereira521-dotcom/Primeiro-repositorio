c62b51a8c53d0d4f2335924a0a57197d9f4e7898
sthefanypereira <sthefanypereira521@gmail.com>
2026-03-01 13:05:48 -0300

    Atualiza exercícios de if else

--- 02_if_else/jogo  de adivinhaçao.py
+++ 02_if_else/jogo  de adivinhaçao.py
@@ -1,10 +1,10 @@
 from random import randint
-computador = randint(0,5)
-print('-=-' * 20) 
+computador = randint(0, 5)
+print('-=-' * 20)
 print('vou pensar em um numero entre 0 e 5 . tente adivinhar ....')
 print('-=-' * 20)
-jogador = int(input('em que numero eu pensei ?')) 
+jogador = int(input('em que numero eu pensei ?'))
 if jogador == computador:
-    print('parabens voçe conseguiu')
+    print('parabens voce conseguiu')
 else:
-    print('ganhei!!! eu pensei no  {} e nao no  {}: '.format(computador,jogador))
+    print('ganhei!!! eu pensei no  {} e nao no  {}: '.format(computador, jogador))
